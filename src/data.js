f82ff08585654684267821f5bf14c4bc345a554e
Stacey Mosier <mosierdesign@gmail.com>
2018-03-26 20:01:13 -0400

    Quilt form setup

--- src/data.js
+++ src/data.js
@@ -1,4 +1,9 @@
-let qsizes = [
+
+/* seam allowance */
+const seam = 0.25;
+
+/* set up quilt size */
+let quiltsizes = [
   { text: "Twin", value: [63, 87] },
   { text: "Twin XL", value: [63, 92] },
   { text: "Full", value: [ 78, 87] },
@@ -7,4 +12,16 @@ let qsizes = [
   { text: "California King", value: [ 96,96] }
 ];
 
-let qsizeList = document.getElementById("qsize");
+/* method to make 2 or 4 HST */
+let hstMethods = [
+  { text: "2 HST", value: 2 },
+  { text: "4 HST", value: 4 }
+]
+
+/* user input for quilt */
+let startBlockSize;
+let qsizeList = document.getElementById("quilt-size");
+let hstMethodType = document.getElementById("hst-method-type");
+let bordersize = document.getElementById("border-size");
+let columns = document.getElementById("column-count");
+let rows = document.getElementById("row-count");
