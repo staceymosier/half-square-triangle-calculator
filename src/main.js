2c527a9412359a21a39c78f3ad67c1c8dc6d94a0
Stacey Mosier <mosierdesign@gmail.com>
2018-03-27 23:05:06 -0400

    removing lodash from demo

--- src/main.js
+++ src/main.js
@@ -1,11 +1,2 @@
-import _ from 'lodash';
 import form from './form.js';
-
-function component() {
-  var element = document.createElement('div');
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  return element;
-}
-
-document.body.appendChild(component());
 form.buildForm();
