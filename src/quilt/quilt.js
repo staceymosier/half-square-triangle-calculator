6b86bd14c5293aa9da6a8aec541ac238cea7b967
Stacey Mosier <mosierdesign@gmail.com>
2018-03-29 23:53:53 -0400

    Comments

--- src/quilt/quilt.js
+++ src/quilt/quilt.js
@@ -1,5 +1,14 @@
 import hst from "../util/math.js";
 
+function round(value, decimals) {
+  // http://www.jacklmoore.com/notes/rounding-in-javascript/
+  return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
+}
+
+// reference
+// 1/16, 1/8, 3/16, 1/4, 5/16, 3/8, 7/16, 1/2
+// 9/16, 5/8, 11/16, 3/4, 13/16, 7/8, 15/16, 1
+
 const buildQuilt = () => {
 
   /* elems */
@@ -9,7 +18,8 @@ const buildQuilt = () => {
 
   /* math */
   let result = hst.doMath( parseInt(startBlockSize), parseInt(hstElem));
-  let result2Frac = hst.toFraction(result);
+  let rounded = round(result, 3);
+  let result2Frac = hst.toFraction(rounded);
 
   /* reset previous messages */
   resultParent.innerHTML = "";
