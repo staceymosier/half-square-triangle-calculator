91242c6f069b20b3a3197ab09a7d7121009fa483
Stacey Mosier <mosierdesign@gmail.com>
2018-03-31 11:58:32 -0400

    adding favicon

--- src/util/math.js
+++ src/util/math.js
@@ -35,9 +35,16 @@ const doMath = (startsize, hstmethod) => {
 
 const toFraction = (size) => {
   let whole = Math.floor(size);
-  let decimal = round(size - whole, 3);     console.log('decimal ' + decimal);
-  let matches = cleanFractions.filter(value => value <= decimal); console.log(matches);
-  let match = matches[matches.length - 1];     console.log('match ' + match);
+  
+  let decimal = round(size - whole, 3);
+  console.log('decimal ' + decimal);
+
+  let matches = cleanFractions.filter(value => value <= decimal);
+  console.log(matches);
+
+  let match = (matches.length == 0 ? 0 : matches[matches.length - 1]);
+  console.log('match ' + match);
+
   let answer = whole + ' ' + n2f(match);   console.log(n2f(match));
   return answer;
 };
