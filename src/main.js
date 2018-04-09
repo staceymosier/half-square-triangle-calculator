b0e411804ced8b5f219c07150985a7de36972868
Stacey Mosier <mosierdesign@gmail.com>
2018-04-08 21:06:57 -0400

    Adding NounProject icons and CSS

--- src/main.js
+++ src/main.js
@@ -1,17 +1,9 @@
 import form from './form/form.js';
 import quilt from './quilt/quilt.js';
-import toggle from './util/toggler.js';
 
 form.buildForm();
 
 let button = document.querySelector("#hst-go");
 button.addEventListener("click", ()=> {
   quilt.buildQuilt();
-});
-
-let collapseElem = document.querySelector(".hst-instructions");
-let collapseParent = document.querySelector("#hst-instructions");
-collapseElem.addEventListener("click", ()=> {
-   toggle.toggleState(collapseElem, "on", "off");
-   toggle.toggleState(collapseParent, "open", "closed");
 });
