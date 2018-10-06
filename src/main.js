38eec7d3296534c72893e1d2bc93b76a89f08628
Stacey Mosier <mosierdesign@gmail.com>
2018-10-06 16:56:38 -0400

    Add jtest

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
+// https://use.fontawesome.com/releases/v5.0.6/js/all.js
+// ga UA-42436380-3
+// https://www.googletagmanager.com/gtag/js?id=UA-42436380-3
+
 import form from './js/form/form-select-lists';
 import quilt from './js/quilt/quilt.js';
 
