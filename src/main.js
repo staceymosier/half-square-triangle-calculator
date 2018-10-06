780b8f47ab55bf3ddd630093e0ec133dedc724f2
Stacey Mosier <mosierdesign@gmail.com>
2018-10-06 13:02:23 -0400

    Replace webpack with parcel

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
-var test = 1;
+import form from './js/form/form-select-lists';
+import quilt from './js/quilt/quilt.js';
+
+form.buildForm();
+let button = document.querySelector("#hst-go");
+button.addEventListener("click", ()=> {
+  quilt.buildQuilt();
+});
