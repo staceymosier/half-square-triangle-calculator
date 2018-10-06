a6a42af87e0d00c1cf6045949c0606e573b49aa8
Stacey Mosier <mosierdesign@gmail.com>
2018-10-06 12:35:41 -0400

    Refactoring sass setup

--- src/main.js
+++ src/main.js
@@ -1,8 +1,1 @@
-import form from './js/form/form-select-lists';
-import quilt from './js/quilt/quilt.js';
-
-form.buildForm();
-let button = document.querySelector("#hst-go");
-button.addEventListener("click", ()=> {
-  quilt.buildQuilt();
-});
+var test = 1;
